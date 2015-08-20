a4030df3cfe854c194956e88a1b8444c6221d5c3
jedieudonne <jedieudonne@yahoo.fr>
2015-08-20 09:34:28 +0200

    project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # getting-and-cleaning-data-project
 
 this is the run analysis programming assignement. 
-the program is divided in 10 parts :
+the program is divided in 12 parts :
 
 # 1st part : getting data
 connect to the url, download the files. 
@@ -9,32 +9,29 @@ connect to the url, download the files.
 Note :  this part can be omitted because the text of the assignement says "code should have a file run_analysis.R in the main directory that can be run as long as the Samsung data is in your working directory".
 I think this part is usefull, so I let it in my final submission.
 
-# 2nd part : subsetting "features"
+# 2nd part : reading data
+reads every file necessary to the programm.
+
+# 3rd part : subsetting "features"
 selects the features with mean and std.
 
-# 3rd part 1 : subsetting "train"
-reads train and subset following the features 
+# 4th part 1 : subsetting "train"
+subsets iun accordance with the features 
 
-# 3rd part 2 : subsetting "test"
-reads test and subset following the features 
+# 5th part 2 : subsetting "test"
+tests and subsets in accordance with the features 
 
-# 4th part : merging and naming
+# 6th part : merging and naming
 concatenate train and test, and gives names to the columns.
-
-# 5th part : reading all about activities  
-reads about activities.                        
-
-# 6th part : creating final.activity
+             
+# 7th part : creating final_activity
 concatenate activity files.
 
-# 7th part : reading all about subjects
-reads about activities.
-
-# 8th part : creating final.subjects
+# 8th part : creating final_subjects
 concatenate activity files.
 
 # 9th part : grouping
-add activities and ids to the data.frame, 
+add activities and ids to the data_frame, 
 
 # 10th part : creating final file
 create the file.
@@ -42,4 +39,4 @@ create the file.
 # 11th part : cleaning 
 cleans every internal variable.
 
-In this course I read many things on internet. Special thanks to rdpeng, benjamin-chan, glynn, sdrdis, and many others. Their point of view on the assignement helped me to construct my own submission.
+In this course I read many things on internet. Special thanks to rdpeng, Dheeraj Agarwal, Gregory D. Horne, benjamin-chan, glynn, sdrdis, and many others. Their point of view on the assignement helped me to construct my own submission.
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,3 @@
-
 ## 1st part : getting data
 url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 filename<-"dossier"
@@ -7,37 +6,34 @@ setwd("C:/Users/jerome/Documents/R")
 data <- unzip(filename)
 setwd("C:/Users/jerome/Documents/R/UCI HAR Dataset")
 
-## 2nd part : subsetting "features"
+##  2nd part reads everything usefull
 feature_names <- read.table("features.txt")
-features_ok <- grep("std|mean", feature_names$V2)
-
-## 3rd part 1 : subsetting "train"
 train_features <- read.table("train/X_train.txt")
-desired_train_features <- train_features[,features_ok]
-
-## 3rd part 2 : subsetting "test"
 test_features <- read.table("test/X_test.txt")
-desired_test_features <- test_features[,features_ok]
-
-## 4th part : merging and naming
-total_features <- rbind(desired_train_features, desired_test_features)
-colnames(total_features) <- feature_names[features_ok, 2]
-
-## 5th part : reading all about activities
 test_activities <- read.table("test/y_test.txt")
 train_activities <- read.table("train/y_train.txt")
 activity_labels <- read.table("activity_labels.txt")
-
-## 6th part : creating final.activity
-final_activity<- rbind(train_activities, test_activities)
-final_activity$activity <- factor(final_activity$V1, levels = activity_labels$V1, labels = activity_labels$V2)
-
-## 7th part : reading all about subjects
 test_subjects <- read.table("test/subject_test.txt")
 train_subjects <- read.table("train/subject_train.txt")
 
+## 3rd part : subsetting "features"
+features_ok <- grep("std|mean", feature_names$V2)
 
-## 8th part : creating final.subjects
+## 4th part 1 : subsetting "train"
+desired_train_features <- train_features[,features_ok]
+
+## 5th part 2 : subsetting "test"
+desired_test_features <- test_features[,features_ok]
+
+## 6th part : merging and naming
+total_features <- rbind(desired_train_features, desired_test_features)
+colnames(total_features) <- feature_names[features_ok, 2]
+
+## 7th part : creating final_activity
+final_activity<- rbind(train_activities, test_activities)
+final_activity$activity <- factor(final_activity$V1, levels = activity_labels$V1, labels = activity_labels$V2)
+
+## 8th part : creating final_subjects
 final_subjects <- rbind(train_subjects, test_subjects)
 
 ## 9th part : grouping
@@ -48,7 +44,7 @@ result_frame <- aggregate(activity_frame[,3:81], by = list(activity_frame$subjec
 colnames(result_frame)[1] <- "subject_id"
 colnames(result_frame)[2] <- "activity"
 
-# 10th part : creating final file
+# 10h part : creating final file
 write.table(result_frame, file="tidy_data_mean_measures.txt", row.names = FALSE)
 
 # 11th part : cleaning 
